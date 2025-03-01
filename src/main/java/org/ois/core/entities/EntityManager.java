5477c58b35ce60d1c067b5da476df6e984d5abdc
assaf <assafattias93@gmail.com>
2025-03-01 11:47:44 +0200

    start EntityManager impl

--- src/main/java/org/ois/core/entities/EntityManager.java
+++ src/main/java/org/ois/core/entities/EntityManager.java
@@ -1,5 +1,17 @@
 package org.ois.core.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EntityManager {
 
+    List<Entity> entities = new ArrayList<>();
+
+    public Entity create(String type) {
+        Entity entity = new Entity(type);
+        entities.add(entity);
+        return entity;
+    }
+
+    public void clear() { this.entities.clear(); }
 }
