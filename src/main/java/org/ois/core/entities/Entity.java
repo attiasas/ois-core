9335726d27e8a6071be3608596c2c7f12efc9d8e
assaf <assafattias93@gmail.com>
2025-03-01 11:48:32 +0200

    add DataObject to create Entity

--- src/main/java/org/ois/core/entities/Entity.java
+++ src/main/java/org/ois/core/entities/Entity.java
@@ -1,10 +1,15 @@
 package org.ois.core.entities;
 
 import org.ois.core.utils.ID;
+import org.ois.core.utils.io.data.DataNode;
+import org.ois.core.utils.io.data.DataObject;
+import org.ois.core.utils.log.Logger;
 
 import java.util.Objects;
 
 public class Entity {
+
+    private final static Logger<Entity> log = Logger.get(Entity.class);
 
     public final String name;
 
@@ -25,6 +30,7 @@ public class Entity {
         if (!this.enabled) {
             return;
         }
+        log.info(String.format("{ Type: '%s', ID: '%s' }", this.name, this.id));
     }
 
     @Override
@@ -45,4 +51,32 @@ public class Entity {
     }
 
     public boolean isEnabled() { return this.enabled; }
+
+    public DataObject<Entity> getBlueprint() {
+        final String className = getClass().getName();
+
+        return new DataObject<>() {
+            @Override
+            public Entity loadData(DataNode data) {
+                if (data == null || !data.contains("type")) {
+                    throw new RuntimeException("entity 'type' not provided");
+                }
+                Entity entity = new Entity(data.getProperty("type").getString());
+
+                entity.setEnabled(data.getProperty("enable").getBoolean());
+
+                return entity;
+            }
+
+            @Override
+            public DataNode convertToDataNode() {
+                DataNode root = DataNode.Object();
+                // Add attributes
+                root.set("class", className);
+                root.set("type", name);
+                root.set("enable", enabled);
+                return root;
+            }
+        };
+    }
 }
