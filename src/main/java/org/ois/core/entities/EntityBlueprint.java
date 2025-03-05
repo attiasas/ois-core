8e06d6de7568eba37b2e9b777f668a5a1acef56f
assaf <assafattias93@gmail.com>
2025-03-05 10:43:33 +0200

    add option to implement custom class for Entity and specify at blueprint

--- src/main/java/org/ois/core/entities/EntityBlueprint.java
+++ src/main/java/org/ois/core/entities/EntityBlueprint.java
@@ -1,11 +1,16 @@
 package org.ois.core.entities;
 
+import org.ois.core.utils.ReflectionUtils;
 import org.ois.core.utils.io.data.Blueprint;
 import org.ois.core.utils.io.data.DataNode;
+
 
 public class EntityBlueprint implements Blueprint<Entity> {
 
     protected final String type;
+
+    protected String customClass;
+    private static final String CUSTOM_CLASS_ATTRIB = "class";
 
     public EntityBlueprint(String type) {
         this.type = type;
@@ -13,16 +18,38 @@ public class EntityBlueprint implements Blueprint<Entity> {
 
     @Override
     public Entity create() {
+        if (isCustomClassBlueprint()) {
+            try {
+                return ReflectionUtils.newInstance(customClass);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
         return new Entity(type);
+    }
+
+    private boolean isCustomClassBlueprint() {
+        return customClass != null && !customClass.isBlank();
     }
 
     @Override
     public EntityBlueprint loadData(DataNode data) {
+
+        if (data.contains(CUSTOM_CLASS_ATTRIB)) {
+            customClass = data.get(CUSTOM_CLASS_ATTRIB).getString();
+        }
+
         return this;
     }
 
     @Override
     public DataNode convertToDataNode() {
-        return DataNode.Object();
+        DataNode root = DataNode.Object();
+
+        if (isCustomClassBlueprint()) {
+            root.set(CUSTOM_CLASS_ATTRIB, customClass);
+        }
+
+        return root;
     }
 }
