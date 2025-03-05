d11efb8993fe64e33263769d16043b8e5c8f8357
assaf <assafattias93@gmail.com>
2025-03-05 17:46:47 +0200

    enable EntityBlueprint extensions

--- src/main/java/org/ois/core/project/Entities.java
+++ src/main/java/org/ois/core/project/Entities.java
@@ -2,14 +2,17 @@ package org.ois.core.project;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.reflect.ReflectionException;
 import org.ois.core.entities.Entity;
 import org.ois.core.entities.EntityBlueprint;
 import org.ois.core.entities.EntityManager;
+import org.ois.core.utils.ReflectionUtils;
 import org.ois.core.utils.io.data.Blueprint;
 import org.ois.core.utils.io.data.DataNode;
 import org.ois.core.utils.io.data.formats.JsonFormat;
 import org.ois.core.utils.log.Logger;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -27,7 +30,7 @@ public class Entities {
         return blueprints.get(type);
     }
 
-    public static void loadBlueprints() {
+    public static void loadBlueprints() throws ReflectionException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         // Entities are optional, make sure the prerequisites are valid before loading
         FileHandle entitiesDir = Gdx.files.internal(ENTITIES_DIRECTORY);
         if (!entitiesDir.exists() || !entitiesDir.isDirectory()) {
@@ -46,18 +49,20 @@ public class Entities {
                 continue;
             }
             String entityType = entityDefDir.name();
-            FileHandle entityBlueprint = entityDefDir.child(String.format("%s.blueprint.ois", entityType));
-            if (!entityBlueprint.exists() || entityBlueprint.isDirectory()) {
+            FileHandle entityBlueprintFile = entityDefDir.child(String.format("%s.blueprint.ois", entityType));
+            if (!entityBlueprintFile.exists() || entityBlueprintFile.isDirectory()) {
                 log.warn(String.format("entity '%s' skipped, can't find valid blueprint at '%s.blueprint.ois'", entityType, entityType));
                 continue;
             }
-            byte[] data = entityBlueprint.readBytes();
+            byte[] data = entityBlueprintFile.readBytes();
             if (data == null) {
                 throw new RuntimeException(String.format("Can't load '%s' blueprint", entityType));
             }
             String rawData = new String(data);
-            log.debug(LOG_TOPIC, String.format("'%s' Blueprint: %s", entityType, rawData));
-            blueprints.put(entityType, JsonFormat.compact().load(new EntityBlueprint(entityType), rawData));
+            DataNode dataNode = JsonFormat.compact().deserialize(rawData);
+            Blueprint<Entity> entityBlueprint = dataNode.contains("blueprint-class") ? ReflectionUtils.newInstance(dataNode.get("blueprint-class").getString()) : new EntityBlueprint(entityType);
+            log.debug(LOG_TOPIC, String.format("'%s' Blueprint (%s): %s", entityType, entityBlueprint.getClass().getName(), rawData));
+            blueprints.put(entityType, entityBlueprint.loadData(dataNode));
         }
         log.debug(LOG_TOPIC, String.format("Loaded '%d' entities blueprints", blueprints.size()));
     }
